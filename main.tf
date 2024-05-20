89a2509af47815a1f108ca2dbc46f7c57c849ba2
urstrulyManoj1 <111749600+urstrulyManoj1@users.noreply.github.com>
2024-05-20 20:21:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_db_instance" "main" {
   engine_version       = "8.0"
   instance_class       = "db.t3.micro"
   username             = "admin"
-  password             = "Admin@123"  # Use a strong password
+  password             = "Admin123"  # Use a strong password
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
 
