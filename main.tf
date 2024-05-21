d92bc9fe206fd02bf1ac999be43a67b0a1957aa0
urstrulyManoj1 <111749600+urstrulyManoj1@users.noreply.github.com>
2024-05-21 11:04:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "aws_instance" "ec2" {
   key_name      = "rsa"
   instance_type = "t2.micro"
   tags = {
-    Name = "ec2-001"
+    Name = "tf-jenkins-ec2"
   }
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
