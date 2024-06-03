f8f3e51d776988235314bfacef602e859db08627
urstrulyManoj1 <111749600+urstrulyManoj1@users.noreply.github.com>
2024-06-03 14:06:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = "12.0.2.0/24" # Choose a valid CIDR block within the VPC range
-  availability_zone = "us-west-1b"
+  availability_zone = "us-west-1c"
   tags = {
     Name = "subnet_pvt"
   }
